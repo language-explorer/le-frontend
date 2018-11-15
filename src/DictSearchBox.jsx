86df845c82a69991736cf05e3d8104da169548c8
Andreas Beckers <andreas.beckers@cologne-intelligence.de>
2018-11-15 06:11:25 +0100

    Layout

--- src/DictSearchBox.jsx
+++ src/DictSearchBox.jsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import "./DictSearchBox.css"
 
 class DictSearchBox extends Component {
+    search(e) {
+        if (e.key === 'Enter') {
+            alert("search " + e.key);
+        }
+    }
     render() {
         return <div id="searchbox">
-                <input />
+                <input onKeyPress={this.search}/>
             </div>;
     }
 }
